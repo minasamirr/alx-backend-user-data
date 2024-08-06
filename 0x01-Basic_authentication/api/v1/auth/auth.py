95b7f727cab1a9691c7a1c9b14844647cefc5772
minasamirr <mina430q@gmail.com>
2024-08-07 00:12:01 +0300

    .

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,14 +22,10 @@ class Auth:
             bool: True if authentication is required, False otherwise
         """
         # If path is None, authentication is required
-        if path is None or not excluded_paths:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
-        for ex_path in excluded_paths:
-            if ex_path.endswith('*'):
-                if path.startswith(ex_path[:-1]):
-                    return False
-            elif ex_path == path or ex_path + '/' == path:
-                return False
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
