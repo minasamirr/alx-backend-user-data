284e19c934ddef24d4938fd4217d09f32414d0ca
minasamirr <mina430q@gmail.com>
2024-08-06 19:32:12 +0300

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Authentication module for the API.
 """
-from flask import request
+import re
 from typing import List, TypeVar
+from flask import request
 
 
 class Auth:
@@ -12,6 +13,8 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Returns the authorization header from the request """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
