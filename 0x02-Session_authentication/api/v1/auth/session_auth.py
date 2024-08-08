f58c6f336e17381de73bb4ce8538d6cbf3362c6b
minasamirr <mina430q@gmail.com>
2024-08-09 02:39:41 +0300

    task 7

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,14 @@
 """ Session authentication module """
 
 import uuid
+from flask import Blueprint, request, jsonify, make_response
 from api.v1.auth.auth import Auth
 from models.user import User
+from api.v1.app import auth
+import os
+
+
+session_auth_blueprint = Blueprint('session_auth', __name__, url_prefix='/api/v1/auth_session')
 
 
 class SessionAuth(Auth):
@@ -35,3 +41,26 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id) if user_id else None
+
+    @session_auth_blueprint.route('/login/', methods=['POST'], strict_slashes=False)
+    def login():
+        """ Login a user based on email and password """
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email:
+            return jsonify({"error": "email missing"}), 400
+        if not password:
+            return jsonify({"error": "password missing"}), 400
+
+        user = User.search(email)
+        if user is None:
+            return jsonify({"error": "no user found for this email"}), 404
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+        session_id = auth.create_session(user.id)
+        response = make_response(user.to_json())
+        response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+        
+        return response
--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
+""" Initialize all routes for the API
 """
 from flask import Blueprint
 
@@ -9,3 +9,9 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 
 User.load_from_file()
+
+from .session_auth import session_auth_blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+app_views.register_blueprint(session_auth_blueprint)
